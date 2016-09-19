05b21c9119e011e4f8cd03b57a7fcdd1d488c51b
Greg Minshall <minshall@acm.org>
2016-09-19 10:52:56 +0300

    *** empty log message ***

--- icalish.el
+++ icalish.el
@@ -42,6 +42,47 @@
                                location recurrence-id rrule
                                summary uid url))
 
+(defun mycal:lexiless (l1 l2)
+  "is L1 less than L2, comparing element by element"
+  (if (< (car l1) (car l2))
+      t
+    (and (= (car l1) (car l2))
+         (mycal:lexiless (cdr l1) (cdr l2)))))
+
+(defun mycal:compare-date (date1 date2)
+  (let ((y1 (nth 2 date1))
+        (y2 (nth 2 date2))
+        (m1 (nth 1 date1))
+        (m2 (nth 1 date2))
+        (d1 (nth 0 date1))
+        (d2 (nth 0 date2)))
+    (or
+     (< y1 y2)
+     (and (= y1 y2)
+          (or
+           (< m1 m2)
+           (and (= m1 m2)
+                (< d1 d2)))))))
+
+(defun mycal:compare-time (time1 time2)
+  (let ((h1 (nth 0 time1))
+        (h2 (nth 0 time2))
+        (m1 (nth 1 time1))
+        (m2 (nth 1 time2)))
+    
+
+(defun mycal:compare-start-date-time (ev1 ev2)
+  "does EV1 start before (or at same time) as EV2?"
+  (let ((date1 (mycal:event-start-date ev1))
+        (date2 (mycal:event-start-date ev2)))
+    (if (mycal:compare-dates date1 date2)
+        t
+      (if (mycal:compare-dates date2 date1)
+          nil
+        (let ((time1 (mycal:event-start-time ev1))
+              (time2 (mycal:event-start-time ev2)))
+          (not (mycal:compare-times date2 date1)))))))
+
 ;; from (cfw:ical-event-get-dates)
 (defun mycal:ical--get-date (event tag)
   "find a TAG in EVENT, and return date and time of it"
@@ -80,7 +121,7 @@
         for e in (icalendar--all-events ical-list)
         for event = (mycal:ical-convert-event e)
         if event
-        if (cfw:event-end-date event)
+        if (mycal:event-end-date event)
         collect event into periods
         else
         collect event into contents
